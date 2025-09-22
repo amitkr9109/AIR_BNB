42fcc419292d28bcd96f5da5ee31fb54a5d30f95
amitkumar2407 <amitkumar9334978554@gmail.com>
2025-09-22 19:26:41 +0530

    air_bnb_clone

--- Backend/src/config/db/db.js
+++ Backend/src/config/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = async function(){
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(`${process.env.MONGODB_URI}/air_bnb`);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.log("error connect to database",error);
