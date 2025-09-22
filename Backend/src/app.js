14da64f02bcffdcad4bcae12b89e15885d174cd1
amitkumar2407 <amitkumar9334978554@gmail.com>
2025-09-22 22:52:29 +0530

    air_bnb_clone

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,10 +12,7 @@ const morgan = require("morgan");
 const ErrorHandler = require("./middlewares/errorHandler.js");
 
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true, 
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
